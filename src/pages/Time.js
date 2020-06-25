0db394e93ecedcf78024099a9d2eae7395d973ae
Davis Raimon <davisraimon@gmail.com>
2020-06-25 21:29:29 +0530

    timer feature add commit

--- src/pages/Time.js
+++ src/pages/Time.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Times from "../components/Times"
 import '../components/Settings.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from 'react-bootstrap'
+import TextField from '@material-ui/core/TextField';
 
 
 class Time extends React.Component {
@@ -10,11 +12,37 @@ class Time extends React.Component {
     this.state = {
     timeLeftInSecond: Number.parseInt(1, 10) * 60,
     currentPhase: false,
+    buttonLabel:"Start",
+    customTime : 1
     }
     this.decreaseTimer = this.decreaseTimer.bind(this);  
     this.onStart = this.onStart.bind(this)
     this.onReset = this.onReset.bind(this)
     this.onChangePhase = this.onChangePhase.bind(this)
+    this.onTimeChange = this.onTimeChange.bind(this)
+    this.onIncrease = this.onIncrease.bind(this)
+    this.onDecrease = this.onDecrease.bind(this)
+    
+  }
+  onIncrease(){
+    this.setState({
+      timeLeftInSecond: this.state.timeLeftInSecond + 60
+    });
+
+  }
+  onDecrease(){
+    if(this.state.timeLeftInSecond!=60){
+    this.setState({
+      timeLeftInSecond: this.state.timeLeftInSecond - 60
+    });}
+  }
+  onTimeChange(e){
+    console.log(e.target.value)
+    if(e.target.value!=''&&e.target.value<=360){
+    this.setState({timeLeftInSecond: Number.parseInt(e.target.value, 10) * 60})
+    this.setState({customTime: e.target.value})
+  
+  }
     
   }
   decreaseTimer() {
@@ -41,27 +69,40 @@ class Time extends React.Component {
     }  
   }
   onReset(){
-    this.setState({timeLeftInSecond: Number.parseInt(3, 10) * 60})
-    this.onChangePhase()
+    this.setState({timeLeftInSecond: Number.parseInt(this.state.customTime, 10) * 60})
+    this.setState({currentPhase:false})
+    this.setState({buttonLabel:"Start"})
     clearInterval(this.timerInterval)
   }
 
   onChangePhase(){
     console.log("Phase Changed")
+    if(!this.state.currentPhase){
+      this.setState({buttonLabel:"Stop"})
+    }else{
+      this.setState({buttonLabel:"Start"})
+    }    
     this.setState({currentPhase:!this.state.currentPhase})
+ 
      
   }
   render() {
     return (
       <div>
 
-        <Times timeLabel='CountDown' timeLeftInSecond={this.state.timeLeftInSecond}></Times> 
-        <div className='settings'>
+        <Times timeLabel='CountDown' timeLeftInSecond={this.state.timeLeftInSecond}></Times>
+                
+        <div className='settings'> 
         <div style={{padding:10}} >
-        <button onClick={this.onStart}>start</button>         
-        <button onClick={this.onReset}>reset</button> 
+        <Button disabled={this.state.currentPhase} style={{marginRight:8}} onClick={this.onIncrease}>+</Button>
+        <Button onClick={this.onStart}>{this.state.buttonLabel}</Button>         
+        <Button style={{marginLeft:8}} onClick={this.onReset}>reset</Button>
+        <Button disabled={this.state.currentPhase} style={{marginLeft:8}} onClick={this.onDecrease}>-</Button> 
         </div>
         </div>
+        
+        <TextField disabled={this.state.currentPhase} defaultValue={1} onChange={this.onTimeChange} style={{width:100,marginLeft:508}} type="number" className="form-control" variant="outlined" />
+        
       </div>
       
     );
